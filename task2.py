5e04bc5181b551afb02746a05adc82874b9420db
YurGon-crypto <118386859+YurGon-crypto@users.noreply.github.com>
2023-08-21 12:19:18 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,27 @@
+# 'Task 1'
+name = 'Yura'
+day = 'Monday'
+print('My name is' + ' '+ name + ' and today is ' + ' ' + day.upper() + ' - the day to learn Phyton')
+print(f'My name is {name}', f' and today is {day.upper()}', '- the day to learn Phyton')
+
+# 'Task 2'
+first_name = "Yura"
+last_name = "Shven"
+
+full_name = first_name + " " + last_name
+
+# Виведення привітання
+print(full_name + ' ' + "Hello!")
+
+
+# 'Task 3'
+a = 3
+b = 5
+
+print(' a + b =' + ' ' + str(a + b))
+print(' b - a =' + ' ' + str(b - a))
+print(' b * a =' + ' ' + str(b * a))
+print(' b / a =' + ' ' + str(b / a))
+print(' b % a =' + ' ' + str(b % a))
+print(' a ** b =' + ' ' + str(a ** b))
+print(' b,a =' + ' ' + str(pow(b, a)))
